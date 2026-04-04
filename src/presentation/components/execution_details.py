1c74da336a8f499f147556068e205dfc8d4f9c81
Nikolas Luiz Schmitt <nikolas.luiz.schmitt@gmail.com>
2026-04-04 13:51:10 -0300

    fix: ajuste de definição de width da imagem

--- src/presentation/components/execution_details.py
+++ src/presentation/components/execution_details.py
@@ -76,7 +76,7 @@ def render_execution_details(
             with st.spinner("Carregando imagem..."):
                 try:
                     image_bytes = asyncio.run(get_image_use_case.execute(execution))
-                    st.image(image_bytes, use_container_width=True)
+                    st.image(image_bytes, width='stretch')
                 except Exception as e:
                     st.error(f"Não foi possível carregar a imagem original.\n\nDetalhe do Erro: {str(e)}")
         else:
