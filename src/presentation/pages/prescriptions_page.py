d1b882b00fac72b32973d35fe1e447d8c91b6cd9
Nikolas Luiz Schmitt <nikolas.luiz.schmitt@gmail.com>
2026-04-04 18:32:18 -0300

    fix: correção nos filtros aplicados na página de prescrições

--- src/presentation/pages/prescriptions_page.py
+++ src/presentation/pages/prescriptions_page.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-from src.domain.entities import ExecutionFilter
+from src.domain.entities import ExecutionFilter, ExecutionType
 from src.presentation.view_models.prescriptions_view_model import PrescriptionsViewModel
 from src.presentation.components.filters import render_execution_selector, render_client_processor_version_filter, render_llm_model_filter
 from src.presentation.components.execution_details import render_execution_details
@@ -47,23 +47,21 @@ def render_prescriptions_page(view_model: PrescriptionsViewModel) -> None:
         )
 
     prompt_filter = None if selected_prompt == "Todos" else selected_prompt
-    execution_filter = ExecutionFilter(
-        prompt=prompt_filter,
-        client_processor_version=selected_version,
-        llm_model=selected_model
-    )
-
-    global_accuracy = view_model.get_global_accuracy(execution_filter)
-
-    evaluated_image_execs = view_model.get_evaluated_image_executions(execution_filter)
-    evaluated_text_execs = view_model.get_evaluated_text_executions(execution_filter)
-
-    image_executions = [e.execution for e in evaluated_image_execs]
-    text_executions = [e.execution for e in evaluated_text_execs]
 
     tab_image, tab_text = st.tabs(["Processamento de Imagem", "Processamento de Texto"])
 
     with tab_image:
+        execution_filter = ExecutionFilter(
+            prompt=prompt_filter,
+            client_processor_version=selected_version,
+            llm_model=selected_model,
+            processing_type=ExecutionType.IMAGE
+        )
+
+        global_accuracy = view_model.get_global_accuracy(execution_filter)
+        evaluated_image_execs = view_model.get_evaluated_image_executions(execution_filter)
+        image_executions = [e.execution for e in evaluated_image_execs]
+
         render_metrics(image_executions, global_accuracy, view_model.calc_time_use_case)
         selected_img_execution = render_execution_selector(image_executions, "presc_img")
 
@@ -78,7 +76,18 @@ def render_prescriptions_page(view_model: PrescriptionsViewModel) -> None:
             )
 
     with tab_text:
-        render_metrics(text_executions, 0.0, view_model.calc_time_use_case)
+        execution_filter = ExecutionFilter(
+            prompt=prompt_filter,
+            client_processor_version=selected_version,
+            llm_model=selected_model,
+            processing_type=ExecutionType.TEXT
+        )
+
+        global_accuracy = view_model.get_global_accuracy(execution_filter)
+        evaluated_text_execs = view_model.get_evaluated_text_executions(execution_filter)
+        text_executions = [e.execution for e in evaluated_text_execs]
+
+        render_metrics(text_executions, global_accuracy, view_model.calc_time_use_case)
         selected_txt_execution = render_execution_selector(text_executions, "presc_txt")
 
         if selected_txt_execution:
